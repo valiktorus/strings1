20ed7c49efc632bc5321cc5cef5582df24dd5aae
valiktorus <valiktorus@mail.ru>
2017-04-14 00:20:43 +0300

    1

--- src/Constants.java
+++ src/Constants.java
@@ -1,3 +1,14 @@
 public class Constants {
     public static final String FILE_NAME = "src/in.csv";
+    public static final String PLUS = " + ";
+    public static final String MINUS = " - ";
+    public static final int FIRST_OPERATOR_START = 7;
+    public static final int FIRST_OPERATOR_END = 10;
+    public static final int FIRST_OPERATOR_INDEX = 8;
+    public static final String INPUT_FILE_IS_NOT_FOUND = "Input file is not found";
+    public static final String NUMBERS_SEPARATOR = ";";
+    public static final String ERROR_LINES = "\nerror-lines = ";
+    public static final String SYMBOLS_AFTER_LAST_NUMBER = ") = ";
+    public static final double DOUBLE_ZERO = 0.0;
+    public static final String RESULT = "result(";
 }
--- src/Runner.java
+++ src/Runner.java
@@ -8,31 +8,48 @@ public class Runner {
         Scanner scanner = null;
         int errorLines = 0;
         double sum = 0;
+        StringBuilder builder = new StringBuilder(Constants.RESULT);
         try {
             scanner = new Scanner(new FileReader(Constants.FILE_NAME));
             scanner.useLocale(Locale.ENGLISH);
             while (scanner.hasNextLine()) {
                 try {
-                    sum += findNumberInLine(scanner);
+                    double number = findNumberInLine(scanner);
+                    sum += number;
+                    if (number >= Constants.DOUBLE_ZERO){
+                        builder.append(Constants.PLUS);
+                        builder.append(number);
+                    }else {
+                        builder.append(Constants.MINUS);
+                        builder.append(-number);
+                    }
                 }catch (ArrayIndexOutOfBoundsException|NumberFormatException e){
                     errorLines++;
                 }
             }
+            builder.append(Constants.SYMBOLS_AFTER_LAST_NUMBER).
+                    append(sum).
+                    append(Constants.ERROR_LINES).
+                    append(errorLines);
+            char firstNumberOperator = builder.charAt(Constants.FIRST_OPERATOR_INDEX);
+            if (firstNumberOperator == '-'){
+                builder.replace(Constants.FIRST_OPERATOR_START, Constants.FIRST_OPERATOR_END, "-");
+            }else if (firstNumberOperator == '+'){
+                builder.replace(Constants.FIRST_OPERATOR_START, Constants.FIRST_OPERATOR_END, "");
+            }
 
+            System.out.println(builder);
         }catch (FileNotFoundException e){
-            System.err.println("Input file is not found");
+            System.err.println(Constants.INPUT_FILE_IS_NOT_FOUND);
         }finally {
             if (scanner != null){
                 scanner.close();
             }
-            System.out.println(sum);
-            System.out.println(errorLines);
         }
-
     }
     public static double findNumberInLine(Scanner scanner){
         String line = scanner.nextLine();
-        String[] values = line.split(";");
+        String[] values = line.split(Constants.NUMBERS_SEPARATOR);
         int indexRequiredNumber = Integer.parseInt(values[0]);
         return Double.parseDouble(values[indexRequiredNumber]);
 
